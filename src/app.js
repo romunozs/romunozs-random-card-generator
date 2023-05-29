ccd57e6046c20ba9b1f7376607bb6b70c709680f
Rodrigo Munoz <romunozs@gmail.com>
2023-05-29 21:36:24 +0200

    Logic at good point, still missing the if statement for the suites. Bootstrap still to complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandomInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getCardSuite(number) {
+  let card = "";
+  switch (number) {
+    case 1:
+      card = "&#x2764;"; //rojo
+      break;
+    case 2:
+      card = "&#x2660;";
+      break;
+    case 3:
+      card = "&#x25C6;"; //rojo
+      break;
+    case 4:
+      card = "&#x2663;"; //negro
+      break;
+  }
+  return card;
+}
+const cardSuiteText = document.getElementById("cardSuite");
+const cardNumberText = document.getElementById("cardNumber");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardSuiteNumber = getRandomInteger(1, 4);
+  if (cardSuiteNumber === 3) {
+    cardSuiteText.classList.add("diamond");
+  }
+  //console.log(cardSuite);
+  let cardSuite = getCardSuite(cardSuiteNumber);
+  let cardNumber = getRandomInteger(0, 12);
+  //console.log(cardNumber);
+  cardNumberText.innerHTML = cardNumber;
+  cardSuiteText.innerHTML = cardSuite;
 };
