bf33db66db7661ede707982a4fa975a258243ded
Rodrigo Munoz <romunozs@gmail.com>
2023-05-30 16:36:41 +0200

    Logic enhaced and button added. Style figured out

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ function getRandomInteger(min, max) {
 
 function assignClassAndContent(ids, number) {
   const classMapping = {
-    1: { className: "heart", card: "&#x2764;" },
+    1: { className: "heart", card: "&#x2665;" },
     2: { className: "spade", card: "&#x2660;" },
     3: { className: "diamond", card: "&#x25C6;" },
     4: { className: "clover", card: "&#x2663;" }
@@ -23,7 +23,21 @@ function assignClassAndContent(ids, number) {
 
   for (let i = 0; i < ids.length; i++) {
     const element = document.getElementById(ids[i]);
+    console.log(element);
     if (element) {
+      // element.classList.remove(...element.classList);
+      // element.classList.add(className);
+      element.classList.forEach(existingClass => {
+        if (
+          existingClass === "heart" ||
+          existingClass === "spade" ||
+          existingClass === "diamond" ||
+          existingClass === "clover"
+        ) {
+          element.classList.remove(existingClass);
+        }
+      });
+
       element.classList.add(className);
       element.innerHTML = card;
     }
@@ -32,10 +46,25 @@ function assignClassAndContent(ids, number) {
 
 const cardIds = ["cardSuite1", "cardSuite2", "cardSuite3", "cardSuite4"];
 const cardNumberText = document.getElementById("cardNumber");
+const dealCardButton = document.getElementById("dealCard");
 
-window.onload = function() {
+function initializeDealCard() {
   let cardSuiteNumber = getRandomInteger(1, 4);
   let cardNumber = getRandomInteger(0, 12);
   cardNumberText.innerHTML = cardNumber;
-  let cardSuiteDefinition = assignClassAndContent(cardIds, cardSuiteNumber);
+
+  assignClassAndContent(cardIds, cardSuiteNumber);
+  if (cardSuiteNumber == 1 || cardSuiteNumber == 3) {
+    cardNumberText.classList.remove("card-title-black");
+    cardNumberText.classList.add("card-title-red");
+  } else {
+    cardNumberText.classList.remove("card-title-red");
+    cardNumberText.classList.add("card-title-black");
+  }
+}
+
+dealCardButton.addEventListener("click", initializeDealCard);
+
+window.onload = function() {
+  initializeDealCard();
 };
